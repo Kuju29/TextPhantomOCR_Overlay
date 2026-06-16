90f292835cb32e4733f26b92e07d09b9b4eafa1a
.★. KuJu -TH- <plan291037@outlook.com>
2026-06-16 22:56:39 +0700

    Update constants.js

--- shared/constants.js
+++ shared/constants.js
@@ -28,6 +28,9 @@ export const DEFAULT_MODE = "lens_text";
 export const DEFAULT_LANG = "en";
 export const DEFAULT_SOURCE = "translated";
 
+/** Safe default for multi-image batches. 0/unlimited overloads small Spaces. */
+export const DEFAULT_MAX_CONCURRENCY = 3;
+
 /** Port name used by the content-script keep-alive connection. */
 export const KEEPALIVE_PORT_NAME = "TP_KEEPALIVE";
 
